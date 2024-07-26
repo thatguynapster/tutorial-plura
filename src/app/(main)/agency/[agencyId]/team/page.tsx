059e77690074b67868dbd452b4205f6301a671cf
Andrew Osei <andrewosei94@gmail.com>
2024-07-26 11:29:56 +0000

    teams page done sub account invites complete

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,7 +1,46 @@
+import { db } from "@/lib/db";
 import React from "react";
+import DataTable from "./data-table";
+import { Plus } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
+import { columns } from "./columns";
+import SendInvitation from "@/components/forms/send-invitation";
 
-const TeamPage = () => {
-  return <div>TeamPage</div>;
+type Props = { params: { agencyId: string } };
+
+const page = async ({ params }: Props) => {
+  const authUser = await currentUser();
+  const teamMembers = await db.user.findMany({
+    where: {
+      Agency: { id: params.agencyId },
+    },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+  if (!authUser) return;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: { id: params.agencyId },
+    include: { SubAccount: true },
+  });
+  if (!agencyDetails) return;
+
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+          Add
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue="name"
+      columns={columns}
+      data={teamMembers}
+    ></DataTable>
+  );
 };
 
-export default TeamPage;
+export default page;
